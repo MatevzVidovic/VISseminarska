506e79daab73c9875a3f8928d13066a7695eeee8
MatevzVidovic <76867992+MatevzVidovic@users.noreply.github.com>
2023-03-03 17:37:13 +0100

    Zdaj je simple kot pasulj

--- parser.py
+++ parser.py
@@ -5,44 +5,20 @@ import pandas as pd
 import ast
 
 
-def dictFinder(array_string):
-    outputList = []
-
-    inJSON = False
-    startIX = -1
-    for i in range(len(array_string)):
-        if array_string[i] == '{':
-            inJSON = True
-            startIX = i
-        elif array_string[i] == '}' and inJSON:
-            outputList.append(array_string[startIX:i+1])
-            inJSON = False
-    return outputList
-
-
-
-
 
 df = pd.read_csv('credits.csv')
 
 for i in range (1000):
-    castCompletelyString = df.loc[i].loc["cast"]
+    castString = df.loc[i].loc["cast"]
 
-
-    #print(castCompletelyString)
-
-    castListOfStrings = dictFinder(castCompletelyString)
 
     #print(castListOfStrings)
 
-    vesCast = []
-    for person in castListOfStrings:
-        personDict = ast.literal_eval(person)
-        vesCast.append(personDict)
+    vesCast = personDict = ast.literal_eval(castString)
     
 
     try:
-        TomHanks = vesCast[0]
+        TomHanks = vesCast[0].get("gender")
     except:
         a = 0
     print(TomHanks)
